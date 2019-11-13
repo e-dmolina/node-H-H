2da36c43ee3195f0eb29161ff6935bec0303339a
Diego <diegomolina1983@gmail.com>
2019-11-13 01:02:12 -0300

    cors tercer intento..

--- src/app.js
+++ src/app.js
@@ -6,11 +6,14 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 
 //Middlewares
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-  });
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
