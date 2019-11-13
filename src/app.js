bddb84ce56501112dff5941d70d889917aa87bdf
Diego <diegomolina1983@gmail.com>
2019-11-13 01:13:34 -0300

    cors intento 4

--- src/app.js
+++ src/app.js
@@ -7,13 +7,12 @@ app.set('port', process.env.PORT || 4000);
 
 //Middlewares
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setheader('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
