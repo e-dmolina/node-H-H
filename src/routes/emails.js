d0d89801d92539b1d7ac463564c4bdeb0fe329ed
Diego Molina <diegomolina1983@gmail.com>
2019-11-15 09:19:03 -0300

    cambie respuesta del post para probar

--- src/routes/emails.js
+++ src/routes/emails.js
@@ -4,6 +4,7 @@ const { enviarEmail } = require('../controllers/emails.controller');
 
 router.route('/')
     .get((req, res) => res.json({message: "todo bien"}))
-    .post(enviarEmail);
+    // .post(enviarEmail);
+    .post((req, res) => res.json({message: "respuesta de post!!"}))
 
 module.exports = router;
