f7003c830dadce00ee00a925e661979e708bda90
Diego Molina <diegomolina1983@gmail.com>
2019-11-27 11:09:54 -0300

    cambie de nodemailer a sendgrid

--- src/controllers/emails.controller.js
+++ src/controllers/emails.controller.js
@@ -1,51 +1,33 @@
-const nodemailer = require('nodemailer');
+const sgmail = require('@sendgrid/mail');
 const emailsCtrl = {};
+
+sgmail.setApiKey(process.env.SENDGRIDKEY);
+
+
 
 emailsCtrl.enviarEmail = async (req, res) =>{
 
     const { nombre, email, mensaje } = req.body;
 
-    contentHTML = `
+    const msg = {
+        to:"diegomolina1983@gmail.com",
+        from:"diegomolina1983@gmail.com",
+        subject:"Formulario de contacto",
+        text:"Envio de msj con sendgrid..",
+        html:`
         <h1>EMail</h1>
         <ul>
             <li>Nombre: ${nombre}</li>
             <li>Email: ${email}</li>
         <ul/>
-        <p>${mensaje}</p>
-    `;
-
-    const trasnporter = nodemailer.createTransport({
-
-        host:'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user:'diegomolina1983@gmail.com',
-            pass:'diegomolina1983'
-        },
-        // tls: {
-        //     rejectUnauthorized: false
-        // }
-    });
-    
-    try{
-
-        const info = await trasnporter.sendMail({
-            from: "diegomolina1983@gmail.com",
-            to: 'diegomolina1983@gmail.com',
-            subject: 'Formulario de contacto',
-            html: contentHTML
-        });
-
-        console.log('Mensaje enviado',info);
-
-        res.json({message:"Email recibido"});
-
+        <p>Menaje: ${mensaje}</p>
+    `
     }
-    catch(error){
-       console.log(error);
-       res.json({error: error});
-    }
-} 
+
+    sgmail.send(msg);
+
+    res.json({mensaje:"Mensaje enviado con éxito!"})
+
+}
 
 module.exports = emailsCtrl;
--- src/routes/emails.js
+++ src/routes/emails.js
@@ -5,6 +5,6 @@ const { enviarEmail } = require('../controllers/emails.controller');
 router.route('/')
     .get((req, res) => res.json({message: "todo bien"}))
     // .post(enviarEmail);
-    .post((req, res) => res.json({message: "respuesta de post!!"}))
+    .post(enviarEmail)
 
 module.exports = router;
